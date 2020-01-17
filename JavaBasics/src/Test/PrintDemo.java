6ae82df6a7504302a63091a24fb6f7db49e7752c
nitin <nitin@DESKTOP-U06483R>
2020-01-17 17:28:45 +0530

    First Commit

---
+++ JavaBasics/src/Test/PrintDemo.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class PrintDemo {
+
+	public static void main(String[] args) 
+	{
+		// First java program to display message.
+		
+		System.out.println("Welcome to Java !!!");
+
+	}
+
+}
